68984fffef0afbd594c886b988b230f3d950263a
Jarh Lewis <jarhra96@gmail.com>
2023-12-13 21:41:50 +0100

    Add personal homepage info

--- app/page.tsx
+++ app/page.tsx
@@ -29,20 +29,20 @@ export default function Home() {
         quantity={100}
       />
       <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        chronark
+        Jarh Lewis
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
+          I'm building global supply chain traceability systems at {" "}
           <Link
             target="_blank"
-            href="https://unkey.dev"
+            href="https://psqr.eu"
             className="underline duration-500 hover:text-zinc-300"
           >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+            PSQR
+          </Link> to solve observability, compliance, and automation challenges across several industries.
         </h2>
       </div>
     </div>
