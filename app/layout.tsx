c3d4ffdf134c146b4fb99a6803b98a16f75b6920
Jarh Lewis <jarhra96@gmail.com>
2023-12-23 13:17:08 +0100

    Update site name to jarh.com

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "jarh.com",
+    template: "%s | jarh.com",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
-    title: "chronark.com",
+    title: "jarh.com",
     description:
       "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    url: "https://jarh.com",
+    siteName: "jarh.com",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://jarh.com/og.png",
         width: 1920,
         height: 1080,
       },
